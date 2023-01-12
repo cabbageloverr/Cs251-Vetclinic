6b5109d5770043a9067ca39194a33a3ffd4ffc46
cabbageloverr <122480510+cabbageloverr@users.noreply.github.com>
2023-01-12 21:50:39 +0700

    Add files via upload

---
+++ button.js
@@ -0,0 +1,39 @@
+function home(){
+    window.location.href = 'homepage.html';
+    
+
+}
+
+function pethis(){
+    window.location.href = 'searchHistory.html';
+}
+
+function medtable(){
+    window.location.href = 'medtable.html';
+    
+
+}
+
+
+function date(){
+    window.location.href = 'datetable.html';
+}
+
+
+function emp(){
+    window.location.href = 'emptable.html';
+}
+	
+function hasown(){
+    window.location.href = 'hasOwner.html';
+}
+	
+function noown(){
+    window.location.href = 'noOwner.html';
+}
+
+function nexttoaddpet(){
+    window.location.href = 'noOwner2.html';
+}
+
+
